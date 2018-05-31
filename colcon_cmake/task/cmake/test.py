8da50cd3c75cb701a381fb16c2463d2dbfe5bbc7
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-05-31 16:19:55 -0700

    update help text for greedy options

--- colcon_cmake/task/cmake/test.py
+++ colcon_cmake/task/cmake/test.py
@@ -27,9 +27,9 @@ class CmakeTestTask(TaskExtensionPoint):
         parser.add_argument(
             '--ctest-args',
             nargs='*', metavar='*', type=str.lstrip,
-            help='Pass arguments to all CTest projects. Every arg starting '
-            'with a dash must be prefixed by a space,\n'
-            'e.g. --ctest-args " -L" label')
+            help='Pass arguments to CTest projects. '
+            'Arguments matching other options must be prefixed by a space,\n'
+            'e.g. --ctest-args " --help"')
 
     async def test(self, *, additional_hooks=None):  # noqa: D102
         pkg = self.context.pkg
