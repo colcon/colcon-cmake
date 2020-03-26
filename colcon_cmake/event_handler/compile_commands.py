630b65c6062e62ea3dddd6ddcf0a298013f45db7
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-03-25 22:15:12 -0700

    only regenerate the workspace-level json file if any of the package-level json files has been updated

--- colcon_cmake/event_handler/compile_commands.py
+++ colcon_cmake/event_handler/compile_commands.py
@@ -1,9 +1,9 @@
 # Copyright 2020 Dirk Thomas
 # Licensed under the Apache License, Version 2.0
 
+import os
 from pathlib import Path
 
-from colcon_core.event.job import JobEnded
 from colcon_core.event.job import JobQueued
 from colcon_core.event.job import JobUnselected
 from colcon_core.event_handler import EventHandlerExtensionPoint
@@ -33,56 +33,79 @@ class CompileCommandsEventHandler(EventHandlerExtensionPoint):
         super().__init__()
         satisfies_version(
             EventHandlerExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
-        self._compile_commands = {}
-        self._delayed_packages = set()
+        self._package_names = set()
 
     def __call__(self, event):  # noqa: D102
         data = event[0]
 
         if isinstance(data, JobQueued) or isinstance(data, JobUnselected):
             # delay loading json for all packages
-            self._delayed_packages.add(data.identifier)
-
-        if isinstance(data, JobEnded):
-            # parse json when a job ended
-            self._add_compile_commands(data.identifier)
-            self._delayed_packages.discard(data.identifier)
+            self._package_names.add(data.identifier)
 
         elif isinstance(data, EventReactorShutdown):
-            # load json for previously delayed packages
-            for package_name in self._delayed_packages:
-                self._add_compile_commands(package_name)
+            # if no package has a json file there is no need for a
+            # workspace-level json file
+            package_level_json_paths = self._get_package_level_json_paths()
+            workspace_level_json_path = self._get_path()
+            if not package_level_json_paths:
+                if workspace_level_json_path.exists():
+                    workspace_level_json_path.unlink()
+                return
 
-            # keep deterministic order independent of aborted/selected packages
+            # if the workspace-level json file is newer than all package-level
+            # json files it doesn't need to be regenerated
+            try:
+                workspace_level_mtime = os.path.getmtime(
+                    str(workspace_level_json_path))
+            except Exception:
+                pass
+            else:
+                for json_path in sorted(package_level_json_paths):
+                    try:
+                        mtime = os.path.getmtime(str(json_path))
+                    except Exception:
+                        continue
+                    if mtime > workspace_level_mtime:
+                        break
+                else:
+                    return
+
+            # collect all package-level json data
             all_compile_commands = []
-            for k in sorted(self._compile_commands.keys()):
-                all_compile_commands += self._compile_commands[k]
+            # keep deterministic order independent of aborted/selected packages
+            for json_path in sorted(package_level_json_paths):
+                compile_commands = self._get_compile_commands(json_path)
+                if compile_commands is not None:
+                    all_compile_commands += compile_commands
 
-            json_path = self._get_path()
+            # generate workspace-level json file or remove if empty
             if all_compile_commands:
                 data = yaml.dump(all_compile_commands, default_flow_style=True)
-
-                with json_path.open('w') as h:
+                with workspace_level_json_path.open('w') as h:
                     h.write(data)
-            elif json_path.exists():
-                json_path.unlink()
+            elif workspace_level_json_path.exists():
+                workspace_level_json_path.unlink()
 
-    def _add_compile_commands(self, package_name):
-        json_path = self._get_path(package_name)
-        if not json_path.exists():
-            return
+    def _get_package_level_json_paths(self):
+        json_paths = set()
+        for package_name in self._package_names:
+            json_path = self._get_path(package_name)
+            if json_path.exists():
+                json_paths.add(json_path)
+        return json_paths
+
+    def _get_compile_commands(self, json_path):
         try:
             compile_commands = yaml.safe_load(json_path.read_bytes())
         except Exception as e:
             logger.warning(
                 "Failed to parse '%s': %s" % (json_path.absolute(), e))
-            return
+            return None
         if not isinstance(compile_commands, list):
             logger.warning(
                 "Data in '%s' is expected to be a list" % json_path.absolute())
-            return
-        if compile_commands:
-            self._compile_commands[package_name] = compile_commands
+            return None
+        return compile_commands
 
     def _get_path(self, package_name=None):
         path = Path(self.context.args.build_base)
