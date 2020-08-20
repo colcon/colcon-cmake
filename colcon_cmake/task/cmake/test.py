9c2ac7d61c98a42cf26a1f7e3eb8915dd2ad603b
Dan Rose <dan@digilabs.io>
2020-08-20 01:38:01 -0500

    Remove unused import

--- colcon_cmake/task/cmake/test.py
+++ colcon_cmake/task/cmake/test.py
@@ -7,7 +7,6 @@ from contextlib import suppress
 from pathlib import Path
 
 from colcon_cmake.task.cmake import CTEST_EXECUTABLE
-from colcon_cmake.task.cmake import get_variable_from_cmake_cache
 from colcon_core.event.test import TestFailure
 from colcon_core.logging import colcon_logger
 from colcon_core.plugin_system import satisfies_version
